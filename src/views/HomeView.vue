dc1136f2ab6aff397502e7fc68667afbf7002c99
Andygggg <andyhello31468@gmail.com>
2023-09-15 19:24:36 +0800

    fix: B push first

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App (Main yes)" />
+    <HelloWorld msg="Welcome to Your Vue.js App (Dev yes)" />
   </div>
 </template>
 
